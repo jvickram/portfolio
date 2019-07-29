4b8abfb9ca6cab2d77818ee1b83230e27259fb80
Bickey Jaisawal <jvickram2@gmail.com>
2019-07-29 23:39:12 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+           <code>sOmething cOol is cOming soOn........</code>
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
